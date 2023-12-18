a08053a221b013ae68c1dd114519dc4dcef9d42f
kimsumin <tnals1564@naver.com>
2023-12-18 18:00:13 +0900

    5-1. rest-api 구현중

---
+++ Chapter5/src/main/java/com/smsoft/springtour/controller/HotelRoomController.java
@@ -0,0 +1,11 @@
+package com.smsoft.springtour.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HotelRoomController {
+
+    //@GetMapping(path = "/hotels/{hotelId}/rooms/{roomNumber}")
+
+}
---
+++ Chapter5/src/main/java/com/smsoft/springtour/serializer/ToDollarStringSerializer.java
@@ -0,0 +1,15 @@
+package com.smsoft.springtour.serializer;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+
+public class ToDollarStringSerializer extends JsonSerializer<BigDecimal> {
+    @Override
+    public void serialize(BigDecimal bigDecimal, JsonGenerator jsonGenerator, SerializerProvider serializerProvider) throws IOException {
+        jsonGenerator.writeString(bigDecimal.setScale(2).toString());
+    }
+}
