812c3a447244873d29bb795437ed87ca827122bd
kimsumin <tnals1564@naver.com>
2023-12-19 15:16:26 +0900

    5-1. rest-api(2) - 호텔 정보조회(룸 정보, 예약일자 등)

---
+++ Chapter5/src/main/java/com/smsoft/springtour/Chapter5Application.java
@@ -0,0 +1,11 @@
+package com.smsoft.springtour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter5Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Chapter5Application.class, args);
+    }
+}
--- Chapter5/src/main/java/com/smsoft/springtour/controller/HotelRoomController.java
+++ Chapter5/src/main/java/com/smsoft/springtour/controller/HotelRoomController.java
@@ -1,11 +1,38 @@
 package com.smsoft.springtour.controller;
 
+import com.smsoft.springtour.controller.response.HotelRoomResponse;
+import com.smsoft.springtour.domain.HotelRoomType;
+import com.smsoft.springtour.utils.IdGenerator;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Objects;
 
 @RestController
 public class HotelRoomController {
 
-    //@GetMapping(path = "/hotels/{hotelId}/rooms/{roomNumber}")
+    @GetMapping(path = "/hotels/{hotelId}/rooms/{roomNumber}")
+    public HotelRoomResponse getHotelRoomByPeriod(
+            @PathVariable Long hotelId,
+            @PathVariable String roomNumber,
+            @RequestParam(value = "fromDate", required = false) @DateTimeFormat(pattern = "yyyyMMdd") LocalDate fromDate,
+            @RequestParam(value = "toDate", required = false) @DateTimeFormat(pattern = "yyyyMMdd") LocalDate toDate
+    ) {
+        Long hotelRoomId = IdGenerator.create();
+        BigDecimal originalPrice = new BigDecimal("130.00");
+
+        HotelRoomResponse response = HotelRoomResponse.of(hotelRoomId, roomNumber, HotelRoomType.DOUBLE, originalPrice);
+
+        if (Objects.nonNull(fromDate) && Objects.nonNull(toDate)) {
+            fromDate.datesUntil(toDate.plusDays(1)).forEach(date -> response.reservedAt(date));
+        }
+
+        return response;
+    }
 
 }
