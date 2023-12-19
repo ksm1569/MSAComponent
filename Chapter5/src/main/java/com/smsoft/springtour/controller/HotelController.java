f0141d5f2a9422577faf3d1ab844e7c4c89e77aa
kimsumin <tnals1564@naver.com>
2023-12-19 17:38:08 +0900

    5-1.3. GetMapping 어노테이션에 대해서

---
+++ Chapter5/src/main/java/com/smsoft/springtour/controller/HotelController.java
@@ -0,0 +1,23 @@
+package com.smsoft.springtour.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HotelController {
+
+    /*
+    * @GetMapping 어노테이션을 까보자
+    * path, value, name, consumes(Content-type), produces(Accept) -> OR연산
+    * params, headers -> AND연산
+    * */
+    @GetMapping(path = "/hotels")
+    public void getHotels() {
+        System.out.println("getHotels");
+    }
+
+    @GetMapping(path = "/hotels", params = "open")
+    public void getHotelsByOpen() {
+        System.out.println("getHotelsByOpen");
+    }
+}
