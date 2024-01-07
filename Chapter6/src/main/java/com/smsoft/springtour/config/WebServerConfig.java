d1721c248d0d0c2ced0d5a49d4463b79572a9bc6
ksm1569 <tnals1564@naver.com>
2024-01-07 13:53:30 +0900

    6-1.2. WebMvcConfigurer - addArgumentResolvers - 데이터를 특정객체로 변환하는 기능을 사용해보자

--- Chapter6/src/main/java/com/smsoft/springtour/config/WebServerConfig.java
+++ Chapter6/src/main/java/com/smsoft/springtour/config/WebServerConfig.java
@@ -1,12 +1,22 @@
 package com.smsoft.springtour.config;
 
 import com.smsoft.springtour.controller.converter.HotelRoomNumberConverter;
+import com.smsoft.springtour.controller.resolver.ClientInfoArgumentResolver;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
 public class WebServerConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new ClientInfoArgumentResolver());
+    }
+
     @Override
     public void addFormatters(FormatterRegistry registry) {
         registry.addConverter(new HotelRoomNumberConverter());
---
+++ Chapter6/src/main/java/com/smsoft/springtour/controller/ClientInfo.java
@@ -0,0 +1,16 @@
+package com.smsoft.springtour.controller;
+
+import lombok.Getter;
+import lombok.ToString;
+
+@ToString
+@Getter
+public class ClientInfo {
+    private final String channel;
+    private final String clientAddress;
+
+    public ClientInfo(String channel, String clientAddress) {
+        this.channel = channel;
+        this.clientAddress = clientAddress;
+    }
+}
